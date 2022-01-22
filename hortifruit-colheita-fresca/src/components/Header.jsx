e780032887b97ea28565ff92737eaba0b975b293
Wilian Pereira <wilian_pereira92@outlook.com>
2022-01-21 21:15:36 -0300

    adicionando o componente Header

---
+++ hortifruit-colheita-fresca/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeaderContainer } from '../styles/styles';
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <h1>Hortifruti - Colheita Fresca</h1>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
