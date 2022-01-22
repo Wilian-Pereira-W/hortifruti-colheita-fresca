86f5d2efc300c3b420e212038d5156ab2753e024
Wilian Pereira <wilian_pereira92@outlook.com>
2022-01-21 21:55:42 -0300

    Adicionado pagina de produto

---
+++ hortifruit-colheita-fresca/src/pages/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import ProductList from '../components/ProductList';
+
+function Product() {
+  return (
+    <>
+      <Header />
+      <ProductList />
+    </>
+  );
+}
+
+export default Product;
