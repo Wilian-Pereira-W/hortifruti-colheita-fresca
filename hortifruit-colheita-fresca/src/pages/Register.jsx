ca40c190fae7fc8ae61e467589bacee3dd02abcf
Wilian Pereira <wilian_pereira92@outlook.com>
2022-01-21 21:12:14 -0300

    Adicionando pagina de cadastro

---
+++ hortifruit-colheita-fresca/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardRegister from '../components/CardRegister';
+
+function Register() {
+  return (
+    <div>
+      <CardRegister />
+    </div>
+  );
+}
+
+export default Register;
