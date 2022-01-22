9ceeccd229b37a9c13b88ca2b0cc6e2771742a24
Wilian Pereira <wilian_pereira92@outlook.com>
2022-01-21 22:00:23 -0300

    Adicionando a pagina do carrinho de compra

---
+++ hortifruit-colheita-fresca/src/pages/ShoppingCart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cart from '../components/Cart';
+import Header from '../components/Header';
+
+function ShoppingCart() {
+  return (
+    <>
+      <Header />
+      <Cart />
+    </>
+  );
+}
+
+export default ShoppingCart;
