f9845350184bac16def543136a20ce3c63e16af7
Wilian Pereira <wilian_pereira92@outlook.com>
2022-01-21 22:02:28 -0300

    Adicionando a pagina NotFound

---
+++ hortifruit-colheita-fresca/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ContainerNotFound } from '../styles/styles';
+
+function NotFound() {
+  return (
+    <ContainerNotFound />
+  );
+}
+
+export default NotFound;
