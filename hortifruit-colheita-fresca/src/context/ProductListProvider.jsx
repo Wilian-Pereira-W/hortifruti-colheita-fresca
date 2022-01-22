e52860984fa648ee9c76648fd56645c5424d18da
Wilian Pereira <wilian_pereira92@outlook.com>
2022-01-21 21:57:00 -0300

    Removendo espaço

---
+++ hortifruit-colheita-fresca/src/context/ProductListProvider.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect, useMemo } from 'react';
+import { ProductListContext } from './ProductContext';
+import apiHortiFruti from '../services/apiRequest';
+
+function ProductListProvider({ children }) {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    apiHortiFruti
+      .get('/all')
+      .then((response) => setProduct(response.data))
+      .catch((err) => {
+        console.error(`ops! ocorreu um erro${err}`);
+      });
+  }, []);
+
+  const context = useMemo(() => ({
+    product,
+  }), [product]);
+
+  return (
+    <ProductListContext.Provider value={context}>
+      {children}
+    </ProductListContext.Provider>
+  );
+}
+
+ProductListProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ProductListProvider;
